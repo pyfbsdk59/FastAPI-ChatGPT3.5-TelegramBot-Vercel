f2e42338f6a3d399dab4e2850a6a58a265b259e0
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-03-31 13:57:43 +0800

    httpx

--- src/index.py
+++ src/index.py
@@ -1,57 +1,59 @@
-from fastapi import FastAPI, Request, HTTPException
 import os
 import openai
-from telegram import Update, Bot
-from telegram.ext import Dispatcher, MessageHandler, Filters, CallbackContext
+
+import httpx
+from fastapi import FastAPI, Request
+
+
+TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
+BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+
+client = httpx.AsyncClient()
 
 app = FastAPI()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 conversation = []
 
-class ChatGPT:
+class ChatGPT:  
+    
+
     def __init__(self):
+        
         self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default="gpt-3.5-turbo")
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
+
+
 
     def get_response(self, user_input):
         conversation.append({"role": "user", "content": user_input})
-        response = openai.Completion.create(
-            engine=self.model,
-            prompt=self._format_messages(self.messages) + user_input,
-            max_tokens=60,
-        )
-        conversation.append({"role": "assistant", "content": response["choices"][0]["text"]})
-        return response["choices"][0]["text"].strip()
+        
 
-    @staticmethod
-    def _format_messages(messages):
-        messages_str = ""
-        for message in messages:
-            role = message["role"]
-            content = message["content"]
-            messages_str += f"{role}: {content}\n"
-        return messages_str
+        response = openai.ChatCompletion.create(
+	            model=self.model,
+                messages = self.messages
+
+                )
+
+        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+        
+        print("AI回答內容：")        
+        print(response['choices'][0]['message']['content'].strip())
 
 
-bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
-bot = Bot(token=bot_token)
+        
+        return response['choices'][0]['message']['content'].strip()
 
 chatgpt = ChatGPT()
+  
+    
+@app.post("/callback/")
+async def webhook(req: Request):
+    data = await req.json()
+    chat_id = data['message']['chat']['id']
+    text = data['message']['text']
+    ai_reply_response = chatgpt.get_response(text)  
 
-def reply_handler(update: Update, context: CallbackContext):
-    text = update.message.text
-    ai_reply_response = chatgpt.get_response(text)
-    update.message.reply_text(ai_reply_response)
+    await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={ai_reply_response}")
 
-dp = Dispatcher(bot, None)
-dp.add_handler(MessageHandler(Filters.text, reply_handler))
-
-async def webhook_handler(request: Request):
-    if request.method == 'POST':
-        update = Update.de_json(await request.json(), bot)
-        dp.process_update(update)
-        return 'ok'
-
-#if __name__ == '__main__':
-    #app.run(debug=True)
+    return data
